2870a71145e3b2f5b24f7e8c2eba6fdb80ab4d41
mattbukowski <mattbukowski98@gmail.com>
2024-07-28 12:41:26 +0200

    Add function to generate new grid by button

--- javascript.js
+++ javascript.js
@@ -1,19 +1,21 @@
 const divsContainer = document.querySelector(".container")
 const fragment = document.createDocumentFragment()
 
-const btn = document.querySelector("button")
-btn.addEventListener("click", e => {
-    +prompt("Enter positive number up to 100")
-})
-
-
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div")
-    div.classList.add("div")
-    div.addEventListener("mousemove", e => {
-        div.style.backgroundColor = "white"
-    })
-    fragment.appendChild(div)
+function createDivs (){
+    let numDivs = +prompt("Enter positive number up to 100");
+    for (let i = 0; i < (numDivs * numDivs); i++) {
+        const div = document.createElement("div")
+        div.classList.add("div")
+        div.addEventListener("mousemove", e => {
+            div.style.backgroundColor = "white"
+        })
+        fragment.appendChild(div)
+    }
+    
+    divsContainer.appendChild(fragment) 
 }
 
-divsContainer.appendChild(fragment)
+const btn = document.querySelector("button")
+btn.addEventListener("click", e => {
+    createDivs ()
+})
