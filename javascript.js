007852d043378bf9f6996e0641d051c723867bf1
mattbukowski <mattbukowski98@gmail.com>
2024-07-28 12:17:44 +0200

    Add prompt button

--- javascript.js
+++ javascript.js
@@ -1,12 +1,17 @@
 const divsContainer = document.querySelector(".container")
 const fragment = document.createDocumentFragment()
 
+const btn = document.querySelector("button")
+btn.addEventListener("click", e => {
+    +prompt("Enter positive number up to 100")
+})
+
+
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div")
     div.classList.add("div")
-    div.textContent = "hello"
-    div.addEventListener("mouseenter", e => {
-        div.style.backgroundColor = "black"
+    div.addEventListener("mousemove", e => {
+        div.style.backgroundColor = "white"
     })
     fragment.appendChild(div)
 }
