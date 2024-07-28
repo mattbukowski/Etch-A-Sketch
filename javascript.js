4b0d60f92a37080d0b774d8da8286e521e4b666c
mattbukowski <mattbukowski98@gmail.com>
2024-07-28 16:08:32 +0200

    Create proper grid in container and limit divs to 100

--- javascript.js
+++ javascript.js
@@ -1,29 +1,39 @@
 const divsContainer = document.querySelector(".container")
 const fragment = document.createDocumentFragment()
+divsContainer.style.cssText = `width: 800px; height: 800px;`
 
-function createDivs (){
+function createDivs() {
     let numDivs = +prompt("Enter positive number up to 100");
-    for (let i = 0; i < (numDivs * numDivs); i++) {
-        const div = document.createElement("div")
-        div.classList.add("div")
-        div.addEventListener("mousemove", e => {
-            div.style.backgroundColor = "white"
-        })
-        fragment.appendChild(div)
-    }
-    
-    divsContainer.appendChild(fragment) 
+    if (numDivs > 100) {
+        alert ("Give a valid number")
+    } else if (numDivs <= 100) {
+    for (let j = 0; j < numDivs; j++) {
+        const column = document.createElement("div");
+        for (let i = 0; i < numDivs; i++) {
+            const div = document.createElement("div")
+            div.classList.add("div")
+            div.addEventListener("mousemove", e => {
+                div.style.backgroundColor = "white"
+            })
+            squareSize = 800 / numDivs;
+            div.style.cssText = `width: ${squareSize}px; height: ${squareSize}px;`;
+            column.appendChild(div)
+            fragment.appendChild(column)
+        }
+
+        divsContainer.appendChild(fragment)
+    }}
 }
 
-const btn = document.querySelector("button")
-btn.addEventListener("click", e => {
-    removeDivs()
-    createDivs ()
-})
+    const btn = document.querySelector("button")
+    btn.addEventListener("click", e => {
+        removeDivs()
+        createDivs()
+    })
 
-function removeDivs() {
-    const divsToRemove = document.querySelector(".container");
-    while (divsToRemove.hasChildNodes()) {
-        divsToRemove.removeChild(divsToRemove.firstChild);
+    function removeDivs() {
+        const divsToRemove = document.querySelector(".container");
+        while (divsToRemove.hasChildNodes()) {
+            divsToRemove.removeChild(divsToRemove.firstChild);
+        }
     }
-  }
